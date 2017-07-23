dba5562b7db48c1292d465e440f0db4aec69c395
tmolina27 <tmolina.ing@gmail.com>
2017-07-22 20:37:00 -0400

    titulo

--- application/views/plantilla/Header.php
+++ application/views/plantilla/Header.php
@@ -6,7 +6,7 @@
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-	<title><?php //echo $data['titulo']; ?>  Taller integrado</title>
+	<title><?php //echo $data['titulo']; ?> PPCH</title>
 	 <!-- Bootstrap -->
     <link href="<?php echo base_url() ?>assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">  
 </head>
